fee1d8d4177dd210dea352d54ce321d6d5052011
Lukas Renggli <renggli@gmail.com>
2019-05-30 12:02:24 +0200

    Update example.

--- test/rx_test.dart
+++ test/rx_test.dart
@@ -1,7 +1,9 @@
 library rx.test.core;
 
+import 'package:more/collection.dart' show IntegerRange;
 import 'package:rx/constructors.dart';
 import 'package:rx/core.dart';
+import 'package:rx/operators.dart';
 
 Observer<T> printObserver<T>(String name) => PluggableObserver(
       (value) => print('$name.next($value)'),
@@ -16,18 +18,20 @@ void main() {
 //    }
 //    subscriber.complete();
 //  });
-//  final transformed = observable
-//      .lift(filter((value) => value.isEven))
-//      .lift(map((value) => '${value * value}'))
-//      .lift(filter((value) => value.length < 3))
-//      .lift(take(3));
+  final transformed = fromIterable(IntegerRange(0, 100))
+      .lift(filter((value) => value.isEven))
+      .lift(map((value) => '${value * value}'))
+      .lift(filter((value) => value.length < 3));
 
-  final obs = timer(Duration(seconds: 2), Duration(milliseconds: 500));
-  final subs1 = obs.subscribe(printObserver('1'));
-  final subs2 = obs.subscribe(printObserver('2'));
+  transformed.subscribe(printObserver('One'));
+  transformed.subscribe(printObserver('Two'));
 
-  timer(Duration(seconds: 3))
-      .subscribe(Observer(complete: () => subs1.unsubscribe()));
-  timer(Duration(seconds: 5))
-      .subscribe(Observer(complete: () => subs2.unsubscribe()));
+//  final obs = timer(Duration(seconds: 2), Duration(milliseconds: 500));
+//  final subs1 = obs.subscribe(printObserver('1'));
+//  final subs2 = obs.subscribe(printObserver('2'));
+//
+//  timer(Duration(seconds: 3))
+//      .subscribe(Observer(complete: () => subs1.unsubscribe()));
+//  timer(Duration(seconds: 5))
+//      .subscribe(Observer(complete: () => subs2.unsubscribe()));
 }
