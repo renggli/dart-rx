6198e34ac8dd2f8efc3e46fd0142e0e0438015b0
Lukas Renggli <renggli@gmail.com>
2019-05-30 12:01:35 +0200

    Bring back scheduler.

--- lib/src/core/scheduler.dart
+++ lib/src/core/scheduler.dart
@@ -4,24 +4,31 @@ import 'dart:async' show Zone;
 
 import 'subscription.dart';
 
-Scheduler scheduler = ZoneScheduler();
-
 typedef Callback = void Function();
 
+typedef IterationCallback = bool Function();
+
 abstract class Scheduler {
+  const Scheduler();
+
+  /// Returns the current time.
   DateTime get now;
 
+  /// Schedules a `callback` to be executed.
   Subscription schedule(Callback callback);
 
+  /// Schedules a `callback` to while returns value is `true`.
+  Subscription scheduleIteration(IterationCallback callback);
+
+  /// Schedules a `callback` to be executed after the specified `duration`.
   Subscription scheduleTimeout(Duration duration, Callback callback);
 
+  /// Schedules a `callback` to be executed periodically every `duration`.
   Subscription schedulePeriodic(Duration duration, Callback callback);
 }
 
-class ZoneScheduler implements Scheduler {
-  final Zone zone;
-
-  ZoneScheduler([Zone zone]) : zone = zone ?? Zone.current;
+class DefaultScheduler extends Scheduler {
+  const DefaultScheduler();
 
   @override
   DateTime get now => DateTime.now();
@@ -29,7 +36,7 @@ class ZoneScheduler implements Scheduler {
   @override
   Subscription schedule(Callback callback) {
     final subscription = ActiveSubscription();
-    zone.scheduleMicrotask(() {
+    Zone.current.scheduleMicrotask(() {
       if (subscription.isSubscribed) {
         callback();
       }
@@ -38,21 +45,44 @@ class ZoneScheduler implements Scheduler {
   }
 
   @override
+  Subscription scheduleIteration(IterationCallback callback) {
+    final subscription = ActiveSubscription();
+    void runner() {
+      if (subscription.isSubscribed) {
+        if (callback()) {
+          schedule(runner);
+        } else {
+          subscription.unsubscribe();
+        }
+      }
+    }
+
+    schedule(runner);
+    return subscription;
+  }
+
+  @override
   Subscription scheduleTimeout(Duration duration, Callback callback) =>
-      TimerSubscription(zone.createTimer(duration, callback));
+      TimerSubscription(Zone.current.createTimer(duration, callback));
 
   @override
   Subscription schedulePeriodic(Duration duration, Callback callback) =>
       TimerSubscription(
-          zone.createPeriodicTimer(duration, (timer) => callback()));
+          Zone.current.createPeriodicTimer(duration, (timer) => callback()));
 }
 
-abstract class SynchronousScheduler extends ZoneScheduler {
-  SynchronousScheduler(Zone zone) : super(zone);
+class ImmediateScheduler extends DefaultScheduler {
+  const ImmediateScheduler() : super();
 
   @override
   Subscription schedule(Callback callback) {
     callback();
+    return const InactiveSubscription();
+  }
+
+  @override
+  Subscription scheduleIteration(IterationCallback callback) {
+    for (; callback(););
     return const InactiveSubscription();
   }
 }
