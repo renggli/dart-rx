5f1ce108cbe682ae12923f15ec63a85484de3ec1
Lukas Renggli <renggli@gmail.com>
2019-06-13 07:25:07 +0200

    Cleanup observables.

--- lib/src/constructors/create.dart
+++ lib/src/constructors/create.dart
@@ -1,8 +1,31 @@
 library rx.constructors.create;
 
 import 'package:rx/src/core/observable.dart';
+import 'package:rx/src/core/observer.dart';
+import 'package:rx/src/core/subscriber.dart';
+import 'package:rx/src/core/subscription.dart';
+
+typedef SubscribeFunction<T> = dynamic Function(Subscriber<T> subscriber);
 
 /// Creates an observable sequence from a specified subscribe method
 /// implementation.
 Observable<T> create<T>(SubscribeFunction<T> subscribeFunction) =>
-    SubscribeObservable<T>(subscribeFunction);
+    _SubscribeObservable<T>(subscribeFunction);
+
+class _SubscribeObservable<T> extends Observable<T> {
+  final SubscribeFunction<T> subscribeFunction;
+
+  _SubscribeObservable(this.subscribeFunction);
+
+  @override
+  Subscription subscribe(Observer<T> observer) {
+    final subscriber = Subscriber<T>(observer);
+    try {
+      final subscription = subscribeFunction(subscriber);
+      subscriber.add(Subscription.of(subscription));
+    } catch (error, stackTrace) {
+      subscriber.error(error, stackTrace);
+    }
+    return subscriber;
+  }
+}
--- lib/src/core/observable.dart
+++ lib/src/core/observable.dart
@@ -5,43 +5,23 @@ import 'operator.dart';
 import 'subscriber.dart';
 import 'subscription.dart';
 
-typedef SubscribeFunction<T> = dynamic Function(Subscriber<T> subscriber);
-
 abstract class Observable<T> {
   Observable<S> lift<S>(Operator<T, S> operator) =>
-      OperatorObservable(this, operator);
+      _OperatorObservable(this, operator);
 
   Subscription subscribe(Observer<T> observer);
 }
 
-class SubscribeObservable<T> extends Observable<T> {
-  final SubscribeFunction<T> subscribeFunction;
-
-  SubscribeObservable(this.subscribeFunction);
-
-  @override
-  Subscription subscribe(Observer<T> observer) {
-    final subscriber = Subscriber<T>(observer);
-    try {
-      final subscription = subscribeFunction(subscriber);
-      subscriber.add(Subscription.of(subscription));
-    } catch (error, stackTrace) {
-      subscriber.error(error, stackTrace);
-    }
-    return subscriber;
-  }
-}
-
-class OperatorObservable<T, S> extends Observable<S> {
+class _OperatorObservable<T, R> extends Observable<R> {
   final Observable<T> source;
-  final Operator<T, S> operator;
+  final Operator<T, R> operator;
 
-  OperatorObservable(this.source, this.operator);
+  _OperatorObservable(this.source, this.operator);
 
   @override
-  Subscription subscribe(Observer<S> observer) {
-    final subscriber = Subscriber<S>(observer);
-    subscriber.add(operator(subscriber, source));
+  Subscription subscribe(Observer<R> observer) {
+    final subscriber = Subscriber<R>(observer);
+    subscriber.add(operator.call(subscriber, source));
     return subscriber;
   }
 }
