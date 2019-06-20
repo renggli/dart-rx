2c8fcb12b543f6bb69b32054121e5dba35123204
Lukas Renggli <renggli@gmail.com>
2019-06-20 17:16:20 -0400

    Cleanup functions

--- lib/src/core/functions.dart
+++ lib/src/core/functions.dart
@@ -1,12 +1,12 @@
 library rx.core.functions;
 
-/// Function type used to pass a value.
+/// Callback type used to pass a value.
 typedef NextCallback<T> = void Function(T value);
 
-/// Function type used to complete a sequence of values with a failure.
+/// Callback type used to complete a sequence of values with a failure.
 typedef ErrorCallback = void Function(Object error, [StackTrace stackTrace]);
 
-/// Function type used to complete a sequence of values with a success.
+/// Callback type used to complete a sequence of values with a success.
 typedef CompleteCallback = void Function();
 
 /// Function type to map from a value of type `T` to a value of type `R`.
@@ -28,8 +28,11 @@ T identityFunction<T>(T argument) => argument;
 /// The constant functions.
 T Function() constantFunction0<T>(T value) => () => value;
 T Function(T1) constantFunction1<T, T1>(T value) => (t1) => value;
+T Function(T1, T2) constantFunction2<T, T1, T2>(T value) => (t1, t2) => value;
 
 /// The throwing functions.
 T Function() throwFunction0<T>(Object throwable) => () => throw throwable;
 T Function(T1) throwFunction1<T, T1>(Object throwable) =>
     (t1) => throw throwable;
+T Function(T1, T2) throwFunction2<T, T1, T2>(Object throwable) =>
+    (t1, t2) => throw throwable;
