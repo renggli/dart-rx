dbeb358e19058af481f8870cce02ccfc27c4d186
Lukas Renggli <renggli@gmail.com>
2019-07-20 14:59:16 +0200

    Cleanup the function types.

--- lib/src/core/functions.dart
+++ lib/src/core/functions.dart
@@ -9,20 +9,23 @@ typedef ErrorCallback = void Function(Object error, [StackTrace stackTrace]);
 /// Callback type used to complete a sequence of values with a success.
 typedef CompleteCallback = void Function();
 
-/// Function type for plain callbacks.
+/// Function type for generic callbacks.
 typedef Callback0 = void Function();
-typedef Callback1<T1> = void Function(T1 t1);
-typedef Callback2<T1, T2> = void Function(T1 t1, T2 t2);
+typedef Callback1<T1> = void Function(T1 arg1);
+typedef Callback2<T1, T2> = void Function(T1 arg1, T2 arg2);
+typedef Callback3<T1, T2, T3> = void Function(T1 arg1, T2 arg2, T3 arg3);
 
-/// Function type to map from a value of type `T` to a value of type `R`.
+/// Function type for generic mapping functions.
 typedef Map0<R> = R Function();
-typedef Map1<T1, R> = R Function(T1 t1);
-typedef Map2<T1, T2, R> = R Function(T1 t1, T2 t2);
+typedef Map1<T1, R> = R Function(T1 arg1);
+typedef Map2<T1, T2, R> = R Function(T1 arg1, T2 arg2);
+typedef Map3<T1, T2, T3, R> = R Function(T1 arg1, T2 arg2, T3 arg3);
 
 /// Function type for a predicate from a value of type `T`.
 typedef Predicate0 = bool Function();
-typedef Predicate1<T1> = bool Function(T1 t1);
-typedef Predicate2<T1, T2> = bool Function(T1 t1, T2 t2);
+typedef Predicate1<T1> = bool Function(T1 arg1);
+typedef Predicate2<T1, T2> = bool Function(T1 arg1, T2 arg2);
+typedef Predicate3<T1, T2, T3> = bool Function(T1 arg1, T2 arg2, T3 arg3);
 
 /// The null function.
 void nullFunction() {}
@@ -31,13 +34,17 @@ void nullFunction() {}
 T identityFunction<T>(T argument) => argument;
 
 /// The constant functions.
-T Function() constantFunction0<T>(T value) => () => value;
-T Function(T1) constantFunction1<T, T1>(T value) => (t1) => value;
-T Function(T1, T2) constantFunction2<T, T1, T2>(T value) => (t1, t2) => value;
+Map0<R> constantFunction0<R>(R value) => () => value;
+Map1<T1, R> constantFunction1<T1, R>(R value) => (arg1) => value;
+Map2<T1, T2, R> constantFunction2<T1, T2, R>(R value) => (arg1, arg2) => value;
+Map3<T1, T2, T3, R> constantFunction3<T1, T2, T3, R>(R value) =>
+    (arg1, arg2, arg3) => value;
 
 /// The throwing functions.
-T Function() throwFunction0<T>(Object throwable) => () => throw throwable;
-T Function(T1) throwFunction1<T, T1>(Object throwable) =>
-    (t1) => throw throwable;
-T Function(T1, T2) throwFunction2<T, T1, T2>(Object throwable) =>
-    (t1, t2) => throw throwable;
+Map0<R> throwFunction0<R>(Object throwable) => () => throw throwable;
+Map1<T1, R> throwFunction1<T1, R>(Object throwable) =>
+    (arg1) => throw throwable;
+Map2<T1, T2, R> throwFunction2<T1, T2, R>(Object throwable) =>
+    (arg1, arg2) => throw throwable;
+Map3<T1, T2, T3, R> throwFunction3<T1, T2, T3, R>(Object throwable) =>
+    (arg1, arg2, arg3) => throw throwable;
