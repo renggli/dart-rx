6eff47cf568da2aa6e278ea61e3ee86c9c652f9e
Lukas Renggli <renggli@gmail.com>
2019-07-21 12:50:45 +0200

    Include the pipe tests with the other tests.

--- test/all_test.dart
+++ test/all_test.dart
@@ -4,6 +4,7 @@ import 'package:test/test.dart';
 
 import 'constructors_test.dart' as constructors_test;
 import 'operators_test.dart' as operators_test;
+import 'pipe_test.dart' as pipe_test;
 import 'schedulers_test.dart' as schedulers_test;
 import 'subscriptions_test.dart' as subscriptions_test;
 import 'testing_test.dart' as testing_test;
@@ -11,6 +12,7 @@ import 'testing_test.dart' as testing_test;
 void main() {
   group('constructors', constructors_test.main);
   group('operators', operators_test.main);
+  group('pipe', pipe_test.main);
   group('schedulers', schedulers_test.main);
   group('subscriptions', subscriptions_test.main);
   group('testing', testing_test.main);
