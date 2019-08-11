e94c18ca8c54b42ff574aad3a41e0f8160e41d30
Lukas Renggli <renggli@gmail.com>
2019-08-11 09:09:19 +0200

    Simplify the finalize operator.

--- lib/src/operators/finalize.dart
+++ lib/src/operators/finalize.dart
@@ -2,12 +2,12 @@ library rx.operators.finalize;
 
 import 'package:rx/src/core/operator.dart';
 import 'package:rx/src/shared/functions.dart';
-import 'package:rx/src/subscriptions/anonymous.dart';
+import 'package:rx/subscriptions.dart';
 
 /// Returns an Observable that mirrors the source Observable, but will call a
 /// specified function when the source terminates on complete or error.
 OperatorFunction<T, T> finalize<T>(CompleteCallback finalize) =>
     (source) => source.lift((source, subscriber) {
-          subscriber.add(AnonymousSubscription(finalize));
+          subscriber.add(Subscription.create(finalize));
           return source.subscribe(subscriber);
         });
