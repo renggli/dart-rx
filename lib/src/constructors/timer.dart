cd8c07264e14ba6735799236136b00b9c3533fb4
Lukas Renggli <renggli@gmail.com>
2019-11-15 22:29:59 +0100

    Fix the timer observable.

--- lib/src/constructors/timer.dart
+++ lib/src/constructors/timer.dart
@@ -5,15 +5,14 @@ import '../core/observer.dart';
 import '../core/scheduler.dart';
 import '../core/subscription.dart';
 import '../schedulers/settings.dart';
-import '../subscriptions/sequential.dart';
 
 /// An [Observable] that starts emitting after `delay` and that emits an ever
 /// increasing numbers after each `period` thereafter.
 Observable<int> timer(
-        {Duration delay = Duration.zero,
-        Duration period,
-        Scheduler scheduler}) =>
-    TimerObservable(delay, period, scheduler ?? defaultScheduler);
+    {Duration delay = Duration.zero, Duration period, Scheduler scheduler}) {
+  ArgumentError.checkNotNull(delay, 'delay');
+  return TimerObservable(delay, period, scheduler ?? defaultScheduler);
+}
 
 class TimerObservable with Observable<int> {
   final Duration delay;
@@ -24,18 +23,19 @@ class TimerObservable with Observable<int> {
 
   @override
   Subscription subscribe(Observer<int> observer) {
-    final subscription = SequentialSubscription();
-    subscription.current = scheduler.scheduleRelative(delay, () {
+    final subscription = Subscription.composite();
+    subscription.add(Subscription.create(() => observer.complete()));
+    subscription.add(scheduler.scheduleRelative(delay, () {
       observer.next(0);
       if (period == null) {
-        observer.complete();
         subscription.unsubscribe();
       } else {
-        var value = 0;
-        subscription.current = scheduler.schedulePeriodic(
-            period, (subscription) => observer.next(++value));
+        var counter = 0;
+        subscription.add(scheduler.schedulePeriodic(period, (subscription) {
+          observer.next(++counter);
+        }));
       }
-    });
+    }));
     return subscription;
   }
 }
