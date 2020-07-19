9e3248ff31de138af9f082cf1bbfde942d7d2483
Lukas Renggli <renggli@gmail.com>
2020-07-19 17:11:02 +0200

    Improve observer API.

--- lib/src/core/observer.dart
+++ lib/src/core/observer.dart
@@ -6,27 +6,46 @@ import '../shared/functions.dart';
 import '../shared/settings.dart';
 
 abstract class Observer<T> implements Disposable {
-  /// An observer with custom handlers.
+  /// An observer with custom handlers:
+  ///
+  /// - [next] is a callback that is called zero or more times with values
+  ///   of type [T].
+  /// - [error] is a callback that is called when the observer terminates
+  ///   with an exception and an optional stack trace. If you do not provide an
+  ///   this callback the error is passed to the [defaultErrorHandler], unless
+  ///   [ignoreErrors] is set to `true`.
+  /// - [complete] is a callback that is called when the observer successfully
+  ///   terminates.
   factory Observer({
     NextCallback<T> next,
     ErrorCallback error,
     CompleteCallback complete,
+    bool ignoreErrors = false,
   }) =>
-      BaseObserver<T>(next ?? nullFunction1, error ?? defaultErrorHandler,
+      BaseObserver<T>(
+          next ?? nullFunction1,
+          error ??
+              (ignoreErrors ? nullFunction1Optional1 : defaultErrorHandler),
           complete ?? nullFunction0);
 
   /// An observer that is only interested in values.
+  ///
+  /// By default errors are passed to the [defaultErrorHandler], unless
+  /// [ignoreErrors] is set to `true`.
   factory Observer.next(NextCallback<T> next, {bool ignoreErrors = false}) =>
       BaseObserver<T>(
           next,
           ignoreErrors ? nullFunction1Optional1 : defaultErrorHandler,
           nullFunction0);
 
-  /// An observer that is only interested in failure.
+  /// An observer that is only interested in errors.
   factory Observer.error(ErrorCallback error) =>
       BaseObserver<T>(nullFunction1, error, nullFunction0);
 
-  /// An observer that is only interested in success.
+  /// An observer that is only interested in completions.
+  ///
+  /// By default all errors are passed to the [defaultErrorHandler], unless
+  /// [ignoreErrors] is set to `true`.
   factory Observer.complete(CompleteCallback complete,
           {bool ignoreErrors = false}) =>
       BaseObserver<T>(
