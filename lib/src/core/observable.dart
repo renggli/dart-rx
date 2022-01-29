662c249f150d387207e099380e4f3d368432e6bc
Lukas Renggli <renggli@gmail.com>
2022-01-29 18:16:35 +0100

    Remove mixin status from `Observable` and `ConnectableObservable`

--- lib/src/core/observable.dart
+++ lib/src/core/observable.dart
@@ -2,7 +2,7 @@ import '../disposables/disposable.dart';
 import 'observer.dart';
 
 // Mixin to make an object observable.
-mixin Observable<T> {
+abstract class Observable<T> {
   /// Subscribes with the provided [observer].
   Disposable subscribe(Observer<T> observer);
 }
--- lib/src/observables/connectable.dart
+++ lib/src/observables/connectable.dart
@@ -2,7 +2,7 @@ import '../../disposables.dart';
 import '../core/observable.dart';
 import '../disposables/disposable.dart';
 
-mixin ConnectableObservable<T> implements Observable<T> {
+abstract class ConnectableObservable<T> implements Observable<T> {
   /// Returns `true` if the observable is connected.
   bool get isConnected;
 
