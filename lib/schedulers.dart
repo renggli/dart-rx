32dcc4abf5eb0ee8c00535152404e443629ef04b
Lukas Renggli <renggli@gmail.com>
2023-01-24 23:29:24 +0100

    Fix linter errors that appeared with Dart 2.19

--- lib/schedulers.dart
+++ lib/schedulers.dart
@@ -1,6 +1,5 @@
 export 'src/schedulers/async.dart';
 export 'src/schedulers/immediate.dart';
 export 'src/schedulers/scheduler.dart';
-export 'src/schedulers/scheduler.dart';
 export 'src/schedulers/settings.dart';
 export 'src/schedulers/zone.dart';
