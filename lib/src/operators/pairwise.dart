2da8e25c14747507916e331873ce15e1ad2140f9
Lukas Renggli <renggli@gmail.com>
2023-08-20 15:02:37 +0200

    Fix documentation issues.

--- lib/src/operators/pairwise.dart
+++ lib/src/operators/pairwise.dart
@@ -2,8 +2,9 @@ import '../../core.dart';
 import '../../disposables.dart';
 
 extension PairwiseOperator<T> on Observable<T> {
-  /// Groups the items emitted by an Observable into [Pair] objects that
-  /// represent the latest pair of items emitted by the source Observable.
+  /// Groups the items emitted by an Observable into a
+  /// [record type](https://dart.dev/language/records) that represent the
+  /// latest pair of items emitted by the source Observable.
   ///
   /// For example `[1, 2, 3, 4].toObservable().pairwise()` yields `(1, 2)`,
   /// `(2, 3)`, and `(3, 4)`.
